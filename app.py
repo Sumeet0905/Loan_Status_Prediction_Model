d29e5f9171bafa59c72538ca0cbc837e2eb1c46b
Sumeet Singh <143834431+Sumeet0905@users.noreply.github.com>
2025-12-20 01:09:20 +0530

    Add missing import for os module

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -75,4 +76,5 @@ def predict():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
 
